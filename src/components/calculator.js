7bbaae52990a362a03a0fc8de22bbdba0c8570ba
Shadowboss109 <theodorebennett3@gmail.com>
2022-10-31 08:09:16 -0500

    layout of keys

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,20 +1,66 @@
 import React from "react"
 
 
-class Keys extends React.Component {
+class Key extends React.Component {
     render() {
       return (
         <button className="key">
-          1
+          {this.props.value}
         </button>
       );
     }
   }
+  
+  function renderKey(i) {
+    return <Key value={i} />;
+  }
+
+  class Board extends React.Component {
+    render() {
+      return (
+        <><div  className="d-flex justify-content-center">
+          <h1>Calculator</h1>
+        </div><div className="d-flex justify-content-center">
+
+            <div className="board">
+              {renderKey('(')}
+              {renderKey(')')}
+              {renderKey('%')}
+              {renderKey('CE')}
+              {renderKey(7)}
+              {renderKey(8)}
+              {renderKey(9)}
+              {renderKey('/')}
+              {renderKey(4)}
+              {renderKey(5)}
+              {renderKey(6)}
+              {renderKey('x')}
+              {renderKey(1)}
+              {renderKey(2)}
+              {renderKey(3)}
+              {renderKey('-')}
+              {renderKey(0)}
+              {renderKey('.')}
+              {renderKey('=')}
+              {renderKey('+')}
+              
+            </div>
+          </div></>
+      );
+    }
+  }
+
+ 
+
+
+
 
 export default function Calculator() {
+
+ 
+
     return (
-      <div>
-        <Keys />
-      </div>
+      <Board/>
+      
     );
   }
